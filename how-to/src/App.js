2477c4ec3d5dd9ade7d92d8eb2769a7555c44c36
thegroovetrain <ericthegroovetrain@gmail.com>
2020-05-21 19:55:11 -0400

    context providers

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute';
 
+import { ArticleContext } from './contexts/ArticleContext';
+import { UserContext } from './contexts/UserContext';
+
 import HomePage from './views/HomePage';
 
 function App() {
+
+    const [articles, setArticles] = useState([]);
+    const [user, setUser] = useState(null);
+    
     return (
+        <ArticleContext.Provider value={articles}>
+        <UserContext.Provider value={user}>
         <Router>
             <div className="App">
                 <Route exact path="/">
@@ -14,6 +23,8 @@ function App() {
                 </Route>
             </div>
         </Router>
+        </UserContext.Provider>
+        </ArticleContext.Provider>
     );
 };
 
