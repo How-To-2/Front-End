842f2b88adb65f5eb6c757849e1ccaac541f3761
Jose Paquian <alostnight1993@outlook.com>
2020-05-22 21:52:17 -0500

    Add Production Build

---
+++ how-to/build/precache-manifest.31d3dc56e533f1d4e1b0155102f9ef8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a0d362fde002e91481c5ecec32de8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904ecb9619a63a9880c4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eb40d330e80fb7ac2cf8",
+    "url": "/static/js/2.abce0a86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.abce0a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904ecb9619a63a9880c4",
+    "url": "/static/js/main.bd455be5.chunk.js"
+  },
+  {
+    "revision": "295bbde0f120726fadc1",
+    "url": "/static/js/runtime-main.6c3293ad.js"
+  }
+]);
